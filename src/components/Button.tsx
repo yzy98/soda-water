887c5d0175c71aad7249af4c13ce438e801a099e
yzy98 <yz186337@163.com>
2024-09-13 17:15:10 +0800

    add: 3D soda-can animations

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+
+import { LinkField } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+
+type Props = {
+  buttonLink: LinkField;
+  buttonText: string | null;
+  className?: string;
+};
+
+export default function Button({ buttonLink, buttonText, className }: Props) {
+  return (
+    <PrismicNextLink
+      className={clsx(
+        "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
+        className,
+      )}
+      field={buttonLink}
+    >
+      {buttonText}
+    </PrismicNextLink>
+  );
+}
---
+++ src/components/ViewCanvas.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+
+type Props = {};
+
+export default function ViewCanvas({}: Props) {
+  return (
+    <Canvas
+      style={{
+        position: "fixed",
+        top: 0,
+        left: "50%",
+        transform: "translateX(-50%)",
+        overflow: "hidden",
+        pointerEvents: "none",
+        zIndex: 0,
+      }}
+      shadows
+      dpr={[1, 1.5]}
+      gl={{ antialias: true }}
+      camera={{
+        fov: 30,
+      }}
+    >
+      <View.Port />
+    </Canvas>
+  );
+}
